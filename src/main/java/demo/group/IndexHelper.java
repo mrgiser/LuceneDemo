45c4825136221df85ee56a922a20d6aca1a3f2f4
hefeng <547669341@qq.com>
2018-10-12 16:48:57 +0800

    分组demo

--- src/main/java/demo/group/IndexHelper.java
+++ src/main/java/demo/group/IndexHelper.java
@@ -54,12 +54,12 @@ public class IndexHelper {
     public void createIndexForGroup(int ID, String author, String content) {
         indexWriter = getIndexWriter();
         document = new Document();
-//IntPoint默认是不存储的
+        //IntPoint默认是不存储的
         document.add(new IntPoint("ID", ID));
-//如果想要在搜索结果中获取ID的值，需要加上下面语句
+        //如果想要在搜索结果中获取ID的值，需要加上下面语句
         document.add(new StoredField("ID", ID));
         document.add(new StringField("author", author, Field.Store.YES));
-//需要使用特定的field存储分组，需要排序及分组的话，要加上下面语句，注意默认SortedDocValuesField也是不存储的
+        //需要使用特定的field存储分组，需要排序及分组的话，要加上下面语句，注意默认SortedDocValuesField也是不存储的
         document.add(new SortedDocValuesField("author", new BytesRef(author)));
         document.add(new StringField("content", content, Field.Store.YES));
         try {
